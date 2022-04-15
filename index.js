0c1d6c4f773ed6370916683a24cab922d1e77f1a
Jeffy Fung <jeffyfungkh@gmail.com>
2022-04-15 19:21:36 +0800

    connect backend to MongoDB

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const {v4: uuidv4} = require('uuid');
 const morgan = require('morgan');
 const cors = require('cors');
 const RemoteStorageContact = require('./models/mongo');
@@ -16,29 +15,6 @@ app.use(cors());
 app.use(express.static('build'))
 app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'));
-
-let persons = [
-  { 
-    "id": 1,
-    "name": "Arto Hellas", 
-    "number": "040-123456"
-  },
-  { 
-    "id": 2,
-    "name": "Ada Lovelace", 
-    "number": "39-44-5323523"
-  },
-  { 
-    "id": 3,
-    "name": "Dan Abramov", 
-    "number": "12-43-234345"
-  },
-  { 
-    "id": 4,
-    "name": "Mary Poppendieck", 
-    "number": "39-23-6423122"
-  }
-];
 
 app.get('/', (req, res) => {
   res.json({ content: 'hi'});
@@ -59,8 +35,8 @@ app.get('/api/persons/:id', (req, res) => {
 });
 
 app.delete('/api/persons/:id', (req, res) => {
-  let targetId = Number(req.params.id);
-  persons = persons.filter(p => p.id !== targetId);
+  // let targetId = Number(req.params.id);
+  // persons = persons.filter(p => p.id !== targetId);
   res.status(204).end();
 })
 
@@ -70,16 +46,14 @@ app.post('/api/persons', (req, res) => {
     return;
   }
 
-  if (persons.find(p => p.name === req.body.name)) {
-    res.status(500).json({ error: "name must be unique"});
-    return;
-  }
+  // if (persons.find(p => p.name === req.body.name)) {
+  //   res.status(500).json({ error: "name must be unique"});
+  //   return;
+  // }
 
-  let newPerson = {
-    id: uuidv4(),
+  let contact = new RemoteStorageContact({
     name: req.body.name,
-    number: req.body.number
-  }
-  persons = persons.concat(newPerson);
-  res.json(newPerson);
+    number: req.body.number,
+  });
+  contact.save().then(newContact => { res.json(newContact) });
 })
